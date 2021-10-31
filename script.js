7c69be46ee99890975d4055c2bd30c70e0cbf8f8
Tylerstratford <tylerstratford@hotmail.com>
2021-10-31 22:29:44 +0100

    functionality completed

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ let lastName = document.getElementById('lastName');
 errorFirstName.hidden = true;
 errorLastName.hidden = true;
 
+
 //Email
 let errorEmail = document.getElementById('errorEmail');
 let email = document.getElementById('email');
 errorEmail.hidden = true;
+let validPassword = false;
+let validPasswordMatch = false;
+
 
 //Age
 let age = document.getElementById('age');
@@ -33,20 +37,21 @@ let repeatPassword = document.getElementById('repeatPassword');
 errorPassword.hidden = true;
 errorPasswordMatch.hidden = true;
 
-
-// let firstNameValidated = false;
-// let firstNameValidated = false;
-// let firstNameValidated = false;
-// let firstNameValidated = false;
-// let firstNameValidated = false;
-// let firstNameValidated = false;
-// let firstNameValidated = false;
-// let firstNameValidated = false;
-
-let validated = false;
-
-
 //Functions and event listener***
+
+//Submit
+let submit = document.getElementById('submit');
+submit.disabled = true;
+
+let form = document.getElementById('form')
+
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    console.log('submitted')
+})
+
+
 
 // First name validation
 firstName.addEventListener("keyup", function(e) {
@@ -57,14 +62,14 @@ function validateMinLengthFirstName(element) {
     let regEx = /^(?=(?:[^A-Za-z]*[A-Za-z]){2})(?![^\d~`?!^*¨ˆ;@=$%{}\[\]|\\\/<>#“.,]*[\d~`?!^*¨ˆ;@=$%{}\[\]|\\\/<>#“.,])\S+(?: \S+){0,2}$/;
     if (!regEx.test(element.value)) {
         errorFirstName.hidden = false
-        validated = false;
         firstName.style.border = "1px solid red";
+        submit.disabled = true;
         return false
     } else {
         errorFirstName.hidden = true;
-        console.log(validated);
-        firstName.style.border = "1px solid green"
-        return validated = true;
+        firstName.style.border = "1px solid green";
+        submit.disabled = false
+        return true;
     }
 }
 
@@ -78,16 +83,17 @@ function validateMinLengthLastName(element) {
     let regEx = /^(?=(?:[^A-Za-z]*[A-Za-z]){2})(?![^\d~`?!^*¨ˆ;@=$%{}\[\]|\\\/<>#“.,]*[\d~`?!^*¨ˆ;@=$%{}\[\]|\\\/<>#“.,])\S+(?: \S+){0,2}$/;
     if (!regEx.test(element.value)) {
         errorLastName.hidden = false
-        validated = false;
         lastName.style.border = "1px solid red";
-        return true
+        submit.disabled = true
+        return false
     } else {
         errorLastName.hidden = true;
-        console.log(validated);
         lastName.style.border = "1px solid green"
-        return true;
+        submit.disabled = false
+        return true
     }
 }
+
 
 //Email Validation
 email.addEventListener("keyup", function(e) {
@@ -99,16 +105,18 @@ function validateEmail(element) {
     if (!regEx.test(element.value)) {
         errorEmail.hidden = false;
         email.style.border = "1px solid red";
+        submit.disabled = true;
         return false;
     } else {
         errorEmail.hidden = true;
         email.style.border = "1px solid green"
+        submit.disabled = false
         return true;
     }
 }
 
-//Age Validation
 
+//Age Validation
 age.addEventListener('blur', function(e) {
     calculateAge(e.target);
 })
@@ -129,14 +137,17 @@ function calculateAge(){
         console.log(`${calculatedAge} not old enough`);
         errorAge.hidden = false;
         age.style.border = "1px solid red";
+        submit.disabled = true;
         return false
     } else {
         console.log(`${calculatedAge}  old enough`);
         errorAge.hidden = true;
         age.style.border = "1px solid green"
+        submit.disabled = false
         return true
     }
 }
+
 
 //Address Validation
 //postcal code
@@ -148,13 +159,13 @@ function validatePostalCode(element) {
     let regEx = /^[0-9a-zA-Z]{5}/;
     if (!regEx.test(element.value) || element.value.length > 5) {
         errorPostalCode.hidden = false
-        validated = false;
         postalCode.style.border = "1px solid red";
+        submit.disabled = true;
         return true
     } else {
         errorPostalCode.hidden = true;
-        console.log(validated);
         postalCode.style.border = "1px solid green"
+        submit.disabled = false
         return true;
     }
 }
@@ -168,13 +179,13 @@ function validateCity(element) {
     let regEx = /^(?=(?:[^A-Za-z]*[A-Za-z]){2})(?![^\d~`?!^*¨ˆ;@=$%{}\[\]|\\\/<>#“.,]*[\d~`?!^*¨ˆ;@=$%{}\[\]|\\\/<>#“.,])\S+(?: \S+){0,2}$/;
     if (!regEx.test(element.value)) {
         errorCity.hidden = false
-        validated = false;
         city.style.border = "1px solid red";
+        submit.disabled = true;
         return true
     } else {
         errorCity.hidden = true;
-        console.log(validated);
         city.style.border = "1px solid green"
+        submit.disabled = false
         return true;
     }
 }
@@ -190,13 +201,16 @@ function validatePassword(element) {
     if(!reg.test(element.value)) {
         errorPassword.hidden = false;
         password.style.border = "1px solid red";
+        submit.disabled = true;
         return false
     } else {
         errorPassword.hidden = true;
         password.style.border = "1px solid green"
+        submit.disabled = false
         return true;
     }
 }
+
 
 //Password Match
 repeatPassword.addEventListener("keyup", function(e) {
@@ -207,11 +221,14 @@ function passwordMatch (element) {
     if (password.value !== repeatPassword.value) {
         errorPasswordMatch.hidden = false;
         repeatPassword.style.border = "1px solid red";
+        submit.disabled = true;
         return false
 
     } else {
         errorPasswordMatch.hidden = true;
         repeatPassword.style.border = "1px solid green"
+        submit.disabled = false
         return true
     }
 }
+
